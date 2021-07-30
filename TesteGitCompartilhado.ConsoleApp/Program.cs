3a18da1007f15b46a38cebb6413bd8e72f2cbc7b
Lucas da Silva Costa <lucassilvacosta72@gmail.com>
2021-07-30 12:40:52 -0300

    Add project files.

---
+++ TesteGitCompartilhado.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TesteGitCompartilhado.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
