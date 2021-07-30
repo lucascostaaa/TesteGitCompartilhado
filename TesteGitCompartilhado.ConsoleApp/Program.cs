d26568152a4896cdd709b3ccc76e5d8b8d2d4d21
Lucas da Silva Costa <lucassilvacosta72@gmail.com>
2021-07-30 12:43:37 -0300

    Nome

--- TesteGitCompartilhado.ConsoleApp/Program.cs
+++ TesteGitCompartilhado.ConsoleApp/Program.cs
@@ -7,6 +7,7 @@ namespace TesteGitCompartilhado.ConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Lucas Da Silva Costa");
         }
     }
 }
